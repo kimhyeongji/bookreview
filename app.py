cf563ac36768dd093e751909d89970581d84b098
kimhyeongji <68255210+kimhyeongji@users.noreply.github.com>
2020-08-06 19:37:40 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
+app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만듭니다.
+
+
+## HTML을 주는 부분
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+## API 역할을 하는 부분
+@app.route('/review', methods=['POST'])
+def write_review():
+        title_receive = request.form['title']
+        author_receive=request.form['author']
+        review_receive=request.form['review']
+        print(title_receive,author_receive,review_receive)
+        return jsonify({'result': 'success', 'msg': '저장되었습니다'})
+        doc = { 'title': title, 'author':author, 'review': review}
+
+        db.reviews.insert_one(doc)
+
+
+@app.route('/review', methods=['GET'])
+def read_reviews():
+    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
